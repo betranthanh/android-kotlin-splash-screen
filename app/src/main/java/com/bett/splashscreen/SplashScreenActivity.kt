011f8a42dbfaa4daeafdaa6f0dba00e614513c04
betranthanh <betranthanh@gmail.com>
2018-01-25 04:57:41 +0700

    first commit

---
+++ app/src/main/java/com/bett/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.bett.splashscreen
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
