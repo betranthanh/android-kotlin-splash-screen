4af78fe0113272371498ab4212bbd61866ede22c
Bett <betranthanh@gmail.com>
2018-02-02 14:21:43 +0700

    change to use Handler postDelayed

--- app/src/main/java/com/bett/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/bett/splashscreen/SplashScreenActivity.kt
@@ -1,8 +1,9 @@
 package com.bett.splashscreen
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -10,18 +11,9 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        val background = object : Thread() {
-            override fun run() {
-                try {
-                    Thread.sleep(5000)
-
-                    val intent = Intent(baseContext, MainActivity::class.java)
-                    startActivity(intent)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-        }
-        background.start()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 5000)
     }
 }
